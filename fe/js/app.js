693b0cecebfa2bca835113c36825745c12160900
seoco <osh8777@gmail.com>
2017-06-02 02:02:58 +0900

    first connect server - fe 1. .new-todo input dom : keypress enter -> success create response

--- fe/js/app.js
+++ fe/js/app.js
@@ -1,6 +1,31 @@
 (function (window) {
-	'use strict';
 
-	// Your starting point. Enjoy the ride!
-
+	//input todo script
+	$('.new-todo').on('keypress', function(e) {
+		if (e.which == 13) {
+			alert("you pressed enter key"+$('.new-todo').val());
+			inputTodoAjax($('.new-todo').val());
+		}
+	});
 })(window);
+
+function inputTodoAjax(data) {
+	var todo = {
+		title : data,
+		desc : "null",
+		task : 0
+	};
+	$.ajax({
+		url : "api/todos",
+		dataType : "json",
+		type : "post",
+		data : JSON.stringify(todo),
+		contentType : "application/json; charset=UTF-8", 
+		success : function(data) {
+			alert("Success registration todo");
+		},
+		error : function(request,status,error){
+			alert("code:"+request.status+"\n"+"error:"+error);
+		}
+	});
+}
