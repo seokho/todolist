3bd738e6d94b086d8718ac95cd1cc6eb71a8eb2f
seoco <osh8777@gmail.com>
2017-06-06 23:24:27 +0900

    1. add TodoDao Testcase

--- server/src/test/java/kr/or/connect/todo/persistence/TodoDaoTest.java
+++ server/src/test/java/kr/or/connect/todo/persistence/TodoDaoTest.java
@@ -1,9 +1,12 @@
 package kr.or.connect.todo.persistence;
+import static kr.or.connect.todo.domain.TodoStatus.ACTIVE_TODO;
 import static org.hamcrest.CoreMatchers.*;
 import static org.junit.Assert.*;
 
 import kr.or.connect.todo.domain.Todo;
 import kr.or.connect.todo.domain.TodoStatus;
+import org.junit.Before;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,10 +25,19 @@ import java.util.List;
 public class TodoDaoTest {
     @Autowired
     private TodoDao dao;
+    private Todo todo;
+
+    @Before
+    public void setUp() {
+        todo = new Todo();
+        todo.setTitle("TestBase");
+        todo.setStatus(ACTIVE_TODO.getCode());
+        dao.create(todo);
+    }
 
     @Test
     public void shouldCount() {
-        Integer count = dao.selectByStatus(TodoStatus.ACTIVE_TODO.getCode()).size();
+        Integer count = dao.selectByStatus(ACTIVE_TODO.getCode()).size();
         assertThat(count, is(notNullValue()));
     }
 
@@ -33,5 +45,23 @@ public class TodoDaoTest {
     public void shouldSelectAll() {
         List<Todo> allTodo = dao.selectAll();
         assertThat(allTodo, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldDeleteByStatus() {
+       Integer updateReturnValue = dao.deleteByStatus(ACTIVE_TODO.getCode());
+       assertThat(updateReturnValue, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldCreate() {
+        Todo queryForObjectReturnValue = dao.create(todo);
+        assertThat(queryForObjectReturnValue, is(notNullValue()));
+    }
+
+    @Test
+    public void shouldUpdate() {
+        Integer updateReturnValue = dao.update(todo);
+        assertThat(updateReturnValue, is(notNullValue()));
     }
 }
